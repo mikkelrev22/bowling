97fbef5335ffd2e418b1f46accf719f343775c3d
Michael Thompson <michael.220488@gmail.com>
2020-01-29 13:22:17 -0800

    sketched out functions necessary for game

---
+++ scoring.js
@@ -0,0 +1,23 @@
+
+//array that is the scoreboard
+
+let scoreboard = []
+
+//list of functions
+//function that calculates score each frame accounting for spares and strikes
+
+const calculateScore = () => {
+
+}
+
+//function that 'bowls' a random number between 0 and 10 & adds it to scoreboard
+
+const bowl = () => {
+
+}
+
+//function that identifies when the game is over
+
+const checkGameEnd = () => {
+
+}
