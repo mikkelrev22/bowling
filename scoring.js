fbf60a5bd5d42a6208835b095aa33c9ef572ec28
Michael Thompson <michael.220488@gmail.com>
2020-01-29 15:19:43 -0800

    working on pin score function and testing

--- scoring.js
+++ scoring.js
@@ -13,7 +13,25 @@ const calculateScore = () => {
 //function that 'bowls' a random number between 0 and 10 & adds it to scoreboard
 
 const bowl = () => {
+  //random number between 0 and  10
   return Math.floor(Math.random()*11)
+}
+//function that adds pinScore to frame
+const pinScore = () => {
+  //create currentFrame
+  let frame = []
+  //invoke bowl function to see how many pins were knocked down in first bowl
+  let firstBowl = bowl()
+  //find the number of remaining pins
+  let remainingPins = 10-firstBowl
+  //push first bowl to frame
+  frame.push(firstBowl)
+  if (remainingPins) {
+    let secondBowl = Math.abs(remainingPins - bowl())
+    frame.push(secondBowl)
+  }
+  else {frame.push(0)}
+  scoreboard.push(frame)
 }
 
 //function that identifies when the game is over
@@ -26,5 +44,6 @@ module.exports = {
   Bowl: bowl,
   Scoreboard: scoreboard,
   CalculateScore: calculateScore,
-  CheckGameEnd: checkGameEnd
+  CheckGameEnd: checkGameEnd,
+  PinScore: pinScore
 }
--- scoring.test.js
+++ scoring.test.js
@@ -1,5 +1,5 @@
 const { expect } = require('chai')
-const { Bowl, Scoreboard,  CalculateScore, CheckGameEnd} = require('./scoring.js')
+const { Bowl, Scoreboard,  CalculateScore, CheckGameEnd, PinScore} = require('./scoring.js')
 /*
 Test whether bowl is a function and if returns a number between 0 and 10
 */
@@ -10,4 +10,10 @@ describe('bowl', () => {
   it('should return an integer between 0 and 10, inclusive', ()=>{
     expect(Bowl()).to.be.within(0, 10)
   })
+})
+describe('PinScore', ()=> {
+  it('should be a function', ()=> {
+    expect(PinScore()).to.be.a('function')
+  })
+  it('should ')
 })
