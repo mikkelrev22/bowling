6c4a64243350a4144e7565e2187fbbdb758d6d61
Michael Thompson <michael.220488@gmail.com>
2020-02-02 14:37:19 -0800

    redid App.jsx with useState hook

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -1,18 +1,25 @@
-import React, { Component }  from 'react'
+import React, { useState }  from 'react'
 
-class App extends Component {
-  constructor() {
-    super ()
-    this.state = {
+function Frame ({key, bowl1, bowl2, currentScore}) {
+return <div className="frame">
+{bowl1.text}{bowl2.text}{key.text}{currentScore.text} 
+</div>
+}
 
-    }
-  }
+function App() {
+  const [scoreboard, setScore] = useState([])
+  // const [currentFrame, setCurrentFrame] = useState([]) I don't think this is necessary either
+  // const [bowl, setBowl] = useState(0) I think this may be unneccesary
 
-  render() {
-    return (
-      <div>Hello World</div>
-    )
-  } 
+  return (
+  <div className="app">
+    <div className="scoreboard">
+      {scoreboard.map((frame, i) => (
+      <Frame key={i} bowl1={frame[0]} bowl2={frame[1]} currentScore={frame[2]}/>
+      ))}
+    </div>
+  </div>
+  )
 }
 
 export default App
