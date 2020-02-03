b4ecac783fababb1ce0c5bd61684fe056ba3b4e0
Michael Thompson <michael.220488@gmail.com>
2020-02-03 14:05:23 -0800

    error with how i am setting state

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -1,23 +1,25 @@
 import React, { useState }  from 'react'
 
-const Frame = ({bowl1, bowl2, currentScore}) => {
+const Frame = ({bowl1, bowl2, currentScore, frameNumber}) => {
 return <div className="frame">
-{bowl1}{bowl2}{frameNumber}{currentScore} 
+  {frameNumber+1}
+<div className="bowl">{bowl1} {bowl2}</div>
+<div className="score">{currentScore}</div>
 </div>
 }
 
 const App = () => {
-  const [scoreboard, setScore] = useState([])
+  const [scoreboard, setScore] = useState([[]])
   // const [currentFrame, setCurrentFrame] = useState([]) I don't think this is necessary either
   // const [bowl, setBowl] = useState(0) I think this may be unneccesary
   const Bowl = () => {
-      setScore([...scoreboard, Math.floor(Math.random()*11)])
+      setScore([...scoreboard, scoreboard[0][Math.floor(Math.random()*11)]])
   }
   return (
   <div className="app">
     <div className="scoreboard">
       {scoreboard.map((frame, i) => (
-      <Frame key={i} bowl1={frame[0]} bowl2={frame[1]} currentScore={frame[2]}/>
+      <Frame key={i} bowl1={frame[0]} bowl2={frame[1]} currentScore={frame[0]+frame[1]} frameNumber={i}/>
       ))}
     </div>
     <button className="bowlButton" onClick={Bowl}>Click to bowl!</button>
