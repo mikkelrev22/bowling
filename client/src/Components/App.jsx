faf31f8ea417761312ab0401de48123a1af36a6b
Michael Thompson <michael.220488@gmail.com>
2020-02-03 13:45:19 -0800

    created Bowl method...working on frames

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -1,20 +1,18 @@
 import React, { useState }  from 'react'
 
-const Frame = ({key, bowl1, bowl2, currentScore}) => {
+const Frame = ({bowl1, bowl2, currentScore}) => {
 return <div className="frame">
-{bowl1.text}{bowl2.text}{key.text}{currentScore.text} 
+{bowl1}{bowl2}{frameNumber}{currentScore} 
 </div>
-}
-
-const Bowl = () => {
-   setScore(Math.floor(Math.random()*11))
 }
 
 const App = () => {
   const [scoreboard, setScore] = useState([])
   // const [currentFrame, setCurrentFrame] = useState([]) I don't think this is necessary either
   // const [bowl, setBowl] = useState(0) I think this may be unneccesary
-
+  const Bowl = () => {
+      setScore([...scoreboard, Math.floor(Math.random()*11)])
+  }
   return (
   <div className="app">
     <div className="scoreboard">
@@ -22,7 +20,7 @@ const App = () => {
       <Frame key={i} bowl1={frame[0]} bowl2={frame[1]} currentScore={frame[2]}/>
       ))}
     </div>
-    <button className="bowlButton">Click to bowl!</button>
+    <button className="bowlButton" onClick={Bowl}>Click to bowl!</button>
   </div>
   )
 }
