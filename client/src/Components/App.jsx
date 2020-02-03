26c1ffd94a4fed9c0ef2b4671353663adde66bc5
Michael Thompson <michael.220488@gmail.com>
2020-02-03 12:20:34 -0800

    adding button for bowling

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -1,12 +1,16 @@
 import React, { useState }  from 'react'
 
-function Frame ({key, bowl1, bowl2, currentScore}) {
+const Frame = ({key, bowl1, bowl2, currentScore}) => {
 return <div className="frame">
 {bowl1.text}{bowl2.text}{key.text}{currentScore.text} 
 </div>
 }
 
-function App() {
+const Bowl = () => {
+   setScore(Math.floor(Math.random()*11))
+}
+
+const App = () => {
   const [scoreboard, setScore] = useState([])
   // const [currentFrame, setCurrentFrame] = useState([]) I don't think this is necessary either
   // const [bowl, setBowl] = useState(0) I think this may be unneccesary
@@ -18,6 +22,7 @@ function App() {
       <Frame key={i} bowl1={frame[0]} bowl2={frame[1]} currentScore={frame[2]}/>
       ))}
     </div>
+    <button className="bowlButton">Click to bowl!</button>
   </div>
   )
 }
