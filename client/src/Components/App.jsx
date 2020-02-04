6ab68e06e132d144462ae61cea3ab6fab5e82491
Michael Thompson <michael.220488@gmail.com>
2020-02-03 18:26:10 -0800

    to write: endGame and calcScore funcs

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -3,52 +3,47 @@ import React, { useState }  from 'react'
 const Frame = ({bowl1, bowl2, currentScore, frameNumber}) => {
 return <div className="frame">
   Frame: {frameNumber+1}
-  {bowl1 &&
 <div className="bowl"> Bowl1 Score: {bowl1}</div>
-  } 
-  {bowl2 &&
 <div className="bowl"> Bowl2 Score: {bowl2}</div>
-  } {bowl2 &&
 <div className="score">Frame Score: {currentScore}</div>
-}
 </div>
 }
 
 const App = () => {
-  const [scoreboard, setScore] = useState([])
-  // const [currentFrame, setCurrentFrame] = useState([]) I don't think this is necessary either
-  // const [bowl, setBowl] = useState(0) I think this may be unneccesary
-  const Bowl = () => {
-      let currentFrame = scoreboard.length-1 
-      if (currentFrame < 0) {
-        let bowl1Score = [Math.floor(Math.random()*11)]
-        setScore([...scoreboard, bowl1Score])
-      }
-      else if (scoreboard[currentFrame].length === 1) {
+  const [scoreboard, setScore] = useState([[null, null], [null, null], [null, null], [null, null], [null, null], [null, null], [null, null], [null, null], [null, null], [null, null]])
+  const [currentFrame, setCurrentFrame]  = useState(0)
+  const calculateScore = (scoreboard) => {
+    //create a function that calculates the current running score
+  }
+
+  const endGame = () => {
+
+  }
+
+  const bowl = () => {
+      if (currentFrame === 11) endGame()
+      if (scoreboard[currentFrame][0] !== null) {
         let bowl2Score = Math.floor(Math.random()*(11-scoreboard[currentFrame][0]))
-        let scoreboardCopy = [...scoreboard]
-        scoreboardCopy[currentFrame].push(bowl2Score)
-        setScore(scoreboardCopy)
+        let newScoreboard = JSON.parse(JSON.stringify(scoreboard))
+        newScoreboard[currentFrame][1] = bowl2Score
+        setScore(newScoreboard)
+        setCurrentFrame(currentFrame + 1)
       }
-      else if (scoreboard[currentFrame].length === 2) {
-        let bowl1Score = [Math.floor(Math.random()*11)]
-        setScore([...scoreboard, bowl1Score])
+      else if (scoreboard[currentFrame][0] === null) {
+        let bowl1Score = Math.floor(Math.random()*11)
+        let newScoreboard = JSON.parse(JSON.stringify(scoreboard))
+        newScoreboard[currentFrame][0] = bowl1Score
+        setScore(newScoreboard)
       }
   }
   return (
   <div className="app">
     <div className="scoreboard">
       {scoreboard.map((frame, i) => (
-      <Frame 
-      key={i} 
-      bowl1={frame[0]} 
-      bowl2={frame[1]} 
-      currentScore={frame[0]+frame[1]} 
-      frameNumber={i}
-      />
+      <Frame key={i} bowl1={frame[0]} bowl2={frame[1]} currentScore={frame[0]+frame[1]} frameNumber={i}/>
       ))}
     </div>
-    <button className="bowlButton" onClick={Bowl}>Click to bowl!</button>
+    <button className="bowlButton" onClick={bowl}>Click to bowl!</button>
   </div>
   )
 }
