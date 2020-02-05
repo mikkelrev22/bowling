df200343741a567ec761009cc2566d7589bd1289
Michael Thompson <michael.220488@gmail.com>
2020-02-04 16:01:16 -0800

    score func without strikes and spares completed

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -5,24 +5,36 @@ return <div className="frame">
   Frame: {frameNumber+1}
 <div className="bowl"> Bowl1 Score: {bowl1}</div>
 <div className="bowl"> Bowl2 Score: {bowl2}</div>
-<div className="score">Frame Score: {currentScore}</div>
+<div className="score">Current Score: {currentScore}</div>
 </div>
 }
 
 const App = () => {
   const [scoreboard, setScore] = useState([[null, null], [null, null], [null, null], [null, null], [null, null], [null, null], [null, null], [null, null], [null, null], [null, null]])
   const [currentFrame, setCurrentFrame]  = useState(0)
-  const calculateScore = (scoreboard) => {
-    //create a function that calculates the current running score
+  // const [totalScore, setTotalScore] = useState()
+
+  const calculateScore = (newScoreboard) => {
     let score = 0
-    for (let frame of scoreboard) {
-        score += frame[0] + frame[1]
+    if (currentFrame) {
+      let i = 1;
+      while (i <= currentFrame) {
+        score = newScoreboard[i-1][2] + newScoreboard[i][0] + newScoreboard[i][1] 
+        i++
+      }
     }
-    return score
+    else {
+      score = newScoreboard[0][0] + newScoreboard[0][1]
+    }
+    // console.log(newScoreboard[currentFrame][0], newScoreboard[currentFrame][1], score)
+    newScoreboard[currentFrame][2] = score
+    return newScoreboard
   }
-  const endGame = () => {
-  
-  }
+  // const endGame = () => {
+  //   if (currentFrame === 11) {
+      
+  //   }
+  // }
 
   const bowl = () => {
       if (currentFrame === 11) endGame()
@@ -30,13 +42,16 @@ const App = () => {
         let bowl2Score = Math.floor(Math.random()*(11-scoreboard[currentFrame][0]))
         let newScoreboard = JSON.parse(JSON.stringify(scoreboard))
         newScoreboard[currentFrame][1] = bowl2Score
+        newScoreboard[currentFrame][2] += newScoreboard[currentFrame][1]
         setScore(newScoreboard)
+        calculateScore(newScoreboard)
         setCurrentFrame(currentFrame + 1)
       }
       else if (scoreboard[currentFrame][0] === null) {
         let bowl1Score = Math.floor(Math.random()*11)
         let newScoreboard = JSON.parse(JSON.stringify(scoreboard))
         newScoreboard[currentFrame][0] = bowl1Score
+        calculateScore(newScoreboard)
         setScore(newScoreboard)
       }
   }
@@ -44,7 +59,7 @@ const App = () => {
   <div className="app">
     <div className="scoreboard">
       {scoreboard.map((frame, i) => (
-      <Frame key={i} bowl1={frame[0]} bowl2={frame[1]} currentScore={calculateScore(scoreboard)} frameNumber={i}/>
+      <Frame key={i} bowl1={frame[0]} bowl2={frame[1]} currentScore={frame[2]} frameNumber={i}/>
       ))}
     </div>
     <button className="bowlButton" onClick={bowl}>Click to bowl!</button>
