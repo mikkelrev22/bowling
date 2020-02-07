b15bc16ea832bfcc0e75592c6501fa142b8f186d
Michael Thompson <michael.220488@gmail.com>
2020-02-06 17:19:08 -0800

    using helper functions, refactoring

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -13,82 +13,53 @@ const App = () => {
   const [scoreboard, setScore] = useState([[null, null], [null, null], [null, null], [null, null], [null, null], [null, null], [null, null], [null, null], [null, null], [null, null]])
   const [currentFrame, setCurrentFrame]  = useState(0)
 
-  const calculateScore = (newScoreboard) => {
-    let score = 0
-    if (currentFrame) {
-      let i = currentFrame
-        //add logic for calculating 2 strikes in prev. frames (debug)
-        // if (newScoreboard[i-2][0] === 'X' && newScoreboard[i-1][0] === 'X') {
-        //     newScoreboard[i-2][2] = newScoreboard[i-2][2] + newScoreboard[i-1][2] + newScoreboard[i][2]
-        // }
-        //add logic for calculating 1 strike in prev. frames
-        
-        // if (newScoreboard[i-1][0] === 'X' && newScoreboard[i][1] === 'X') {
-
-        // }
-        if (newScoreboard[i-1][0] === 'X' && newScoreboard[i][1] === '/') {
-          newScoreboard[i-1][2] = newScoreboard[i-1][2] + 10 - newScoreboard[i][0]
-        }
-        else {
-          if (newScoreboard[i][1] === null) newScoreboard[i-1][2] = newScoreboard[i-1][2] + newScoreboard[i][0]
-          if (newScoreboard[i][1] !== null) newScoreboard[i-1][2] = newScoreboard[i-1][2] + newScoreboard[i][1]
-        }
-        //add logic for calculating spares in prev. frames
-        if (newScoreboard[i-1][1] === '/' && newScoreboard[i][1] === null) {
-          if (newScoreboard[i][0] === 'X') {
-            newScoreboard[i-1][2] = newScoreboard[i-1][2] + 10
-          }
-          else {
-            newScoreboard[i-1][2] = newScoreboard[i-1][2] + newScoreboard[i][0]
-          }
-        }
-        
-        //add logic for calculating spares and strikes in current frame
-        if (newScoreboard[i][0] === 'X' || newScoreboard[i][1] === '/') {
-          score = newScoreboard[i-1][2] + 10
-        } 
-        else {
-          score = newScoreboard[i-1][2] + newScoreboard[i][0] + newScoreboard[i][1]
-        }
-    }
-    else if (newScoreboard[0][0] === 'X' || newScoreboard[0][1] === '/') {
-      score = 10
-    }
-    else {
-      score = newScoreboard[0][0] + newScoreboard[0][1]
-    }
-    newScoreboard[currentFrame][2] = score
-    return newScoreboard
+  const calculateScoreBowl1 = (newScoreboard) => {
+    
   }
- 
+  const calculateScoreBowl2 = (newScoreboard) => {
+
+  }
+  
+  const calculateScoreStrike = (newScoreboard) => {
+
+  }
+
+  const checkForStrikes = (newScoreboard) => {
+
+  }
+
+  const checkForSpares = (newScoreboard) => {
+
+  }
+
   const bowl = () => {
       if (scoreboard[currentFrame][0] !== null) {
         let bowl2Score = Math.floor(Math.random()*(11-scoreboard[currentFrame][0]))
-        if (bowl2Score + scoreboard[currentFrame][0] === 10) bowl2Score = '/'
         let newScoreboard = JSON.parse(JSON.stringify(scoreboard))
-        newScoreboard[currentFrame][1] = bowl2Score
-        newScoreboard[currentFrame][2] += newScoreboard[currentFrame][1]
-        setScore(newScoreboard)
-        calculateScore(newScoreboard)
-        setCurrentFrame(currentFrame + 1)
+          newScoreboard[currentFrame][1] = bowl2Score
+          newScoreboard[currentFrame][2] += newScoreboard[currentFrame][1]
+          setScore(newScoreboard)
+          calculateScoreBowl2(newScoreboard)
+          setCurrentFrame(currentFrame + 1)
       }
       else if (scoreboard[currentFrame][0] === null) {
         let bowl1Score = Math.floor(Math.random()*11)
         let newScoreboard = JSON.parse(JSON.stringify(scoreboard))
         newScoreboard[currentFrame][0] = bowl1Score
         if (bowl1Score === 10) {
-          newScoreboard[currentFrame][0] = 'X'
+          newScoreboard[currentFrame][0] = 10
           newScoreboard[currentFrame][1] = null
-          calculateScore(newScoreboard)
+          calculateScoreStrike(newScoreboard)
           setScore(newScoreboard)
           setCurrentFrame(currentFrame + 1)
         }
         else {
-          calculateScore(newScoreboard)
+          calculateScoreBowl1(newScoreboard)
           setScore(newScoreboard)
         }
       }
   }
+
   return (
   <div className="app">
     <div className="scoreboard">
